b6fb482f8e12dd0d6bfdd3cb30c118423c1bb0a1
Moin Khan <nurmoin05@gmail.com>
2022-06-01 23:47:15 +0600

    header structure added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-            
-        </div>
+        <header className="Header">
+            <div className="inner__header container mx-auto">
+                <div className={styles.header__content}>
+                    <div className={styles.header__logo}>
+                        <Link href="/"><a>Moin Khan</a></Link>
+                    </div>
+                    <nav className={styles.header__nav}>
+                        <div className="desktop__menu"></div>
+                        <div className="mobile__menu"></div>
+                    </nav>
+                </div>
+            </div>
+        </header>
     );
 };
 
