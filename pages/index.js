992cbbdd6176468c76d626894fa8ea843526873f
Moin Khan <nurmoin05@gmail.com>
2022-06-01 21:04:12 +0600

    header area starte

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
 
 const Home = () => {
   return (
-    <div className="Home">
+    <section className="Home">
       <Head>
         <title>Moin Khan | Font-End Developer</title>
         <meta name="description" content="Personal Portfolio of Moin Khan" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="inner__home container mx-auto">
+        <main className="home__content">
 
-      <div className="home__container">
-        <h1>This is home</h1>
+        </main>
       </div>
-
-    </div>
+    </section>
   )
 }
 
