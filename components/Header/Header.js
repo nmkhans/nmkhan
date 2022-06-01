870c84cc4d3394bbcca60b59d01b92c4ca8740b0
Moin Khan <nurmoin05@gmail.com>
2022-06-02 00:04:42 +0600

    daisy ui component library added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -11,7 +11,9 @@ const Header = () => {
                         <Link href="/"><a>Moin Khan</a></Link>
                     </div>
                     <nav className={styles.header__nav}>
-                        <div className="desktop__menu"></div>
+                        <div className="desktop__menu">
+                            
+                        </div>
                         <div className="mobile__menu"></div>
                     </nav>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#000000",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
 }
