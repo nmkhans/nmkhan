eea2e127193baa078b16d8fc90ac78fc6ad2c85c
Moin Khan <nurmoin05@gmail.com>
2022-06-03 23:45:28 +0600

    Header compleated

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import ActiveLink from '../../components/ActiveLink/ActiveLink';
+import { FaTimes, FaBars } from 'react-icons/fa';
 
 const Header = () => {
+    const [mobileMenu, setMobilMenu] = useState(false);
+    const [sticky, setSticky] = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            const currentLocation = window.pageYOffset;
+            if(currentLocation > 200) {
+                setSticky(true)
+            } else {
+                setSticky(false)
+            }
+        })
+    }, [])
+
     return (
-        <div className={styles.Header}>
+        <div className={`${styles.Header} ${sticky ? styles.sticky : ""}`}>
             <div className="inner__header container mx-auto">
                 <nav className={styles.header__content}>
                     <div className={styles.header__logo}>
@@ -41,7 +55,34 @@ const Header = () => {
                                 </li>
                             </ul>
                         </div>
-                        <div className={styles.desktop__mobile}></div>
+                        <div className={styles.mobile__menu}>
+                            <div onClick={() => setMobilMenu(!mobileMenu)} className={styles.mobile__icon}>
+                                {
+                                    mobileMenu ? <FaTimes /> : <FaBars />
+                                }
+                            </div>
+                            {
+                                mobileMenu && (
+                                    <ul>
+                                        <li>
+                                            <ActiveLink href={'/'}>Home Mobile</ActiveLink>
+                                        </li>
+                                        <li>
+                                            <ActiveLink href={'/my-projects'}>My Projects</ActiveLink>
+                                        </li>
+                                        <li>
+                                            <ActiveLink href={'/about-me'}>About Me</ActiveLink>
+                                        </li>
+                                        <li>
+                                            <ActiveLink href={'/blog'}>Blog</ActiveLink>
+                                        </li>
+                                        <li>
+                                            <ActiveLink href={'/contact-me'}>Contact Me</ActiveLink>
+                                        </li>
+                                    </ul>
+                                )
+                            }
+                        </div>
                     </div>
                 </nav>
             </div>
