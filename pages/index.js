c083f919056478313bd941c66cedc30f33a5cbec
Moin Khan <nurmoin05@gmail.com>
2022-06-10 09:57:42 +0600

    folder structure completed and header issue udated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Hero from './../components/Hero/Hero';
+import Hero from '../sections/Home/Hero/Hero';
 
 const Home = () => {
   return (
