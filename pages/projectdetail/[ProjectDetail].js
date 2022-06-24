ed70fbdb5b93fc0db08080025683b8b392506b86
Moin Khan <nurmoin05@gmail.com>
2022-06-25 00:31:54 +0600

    single project data fetched from backend

--- pages/projectdetail/[ProjectDetail].js
+++ pages/projectdetail/[ProjectDetail].js
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 
-const singleProject = () => {
+const singleProject = ({projectDetail}) => {
+    console.log(projectDetail)
     return (
         <div>
             <h1>This is project Detail</h1>
@@ -8,4 +10,15 @@ const singleProject = () => {
     );
 };
 
+export const getServerSideProps = async (context) => {
+    const id = context.params.projectDetail;
+    const result = await axios.get(`http://localhost:5000/project/${id}`);
+    const data = result.data;
+
+    return {
+        props: {
+            projectDetail: data
+        }
+    }
+}
 export default singleProject;
