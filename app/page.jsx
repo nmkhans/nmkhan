f133b636f78ff375fcc24cb09e2483555d031f58
nmkhans <nurmoin05@gmail.com>
2023-01-07 21:10:30 +0600

    next js installed and tailwind configured

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Link from 'next/Link'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <section className="Home">
+      
+    </section>
+  )
+}
