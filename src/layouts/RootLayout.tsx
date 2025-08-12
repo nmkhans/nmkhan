060130347faf7c1cf617a7b9d3c2e537bf23917c
nmkhans <nurmoin05@gmail.com>
2025-08-12 15:37:52 +0600

    router added and home page setup

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+const RootLayout = () => {
+  return (
+    <main className="absolute inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+      <Outlet />
+    </main>
+  );
+};
+
+export default RootLayout;
