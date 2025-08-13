b419de94406c83ea69dae5fa9c0afde431437e5e
nmkhans <nurmoin05@gmail.com>
2025-08-13 12:39:41 +0600

    responsive menu added for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
+import MobileMenu from "../MobileMenu/MobileMenu";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="container mx-auto relative top-3">
+    <header className="container mx-auto relative top-3 px-5 lg:px-0">
       <section className="flex justify-between items-center">
-        <nav className="flex justify-center items-center grow-1">
-          <ul className="flex justify-between items-center gap-1 p-0.5 border dark:border-white/15 rounded-full dark:bg-white/10 bg-white backdrop-blur">
+        <div>hello</div>
+        <nav className="hidden lg:flex justify-center items-center grow-1">
+          <ul className="w-1/3 flex justify-between items-center gap-1 p-0.5 border dark:border-white/15 rounded-full dark:bg-white/10 bg-white backdrop-blur">
             <li className="nav-item">
               <a href="">Home</a>
             </li>
@@ -20,7 +22,11 @@ export default function Header() {
             </li>
           </ul>
         </nav>
-        <div>
+
+        <div className="flex items-center gap-1">
+          <div className="inline-block lg:hidden">
+            <MobileMenu />
+          </div>
           <ThemeToggle />
         </div>
       </section>
---
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,43 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "../ui/button";
+import { Menu } from "lucide-react";
+
+export default function MobileMenu() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="outline" size="icon">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>Menu</SheetTitle>
+          <SheetDescription>
+            <ul className="flex flex-col justify-between items-center gap-1 p-0.5 border dark:border-white/15 rounded-md dark:bg-white/10 bg-white backdrop-blur mt-10 py-2">
+              <li className="nav-item">
+                <a href="">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a href="">About</a>
+              </li>
+              <li className="nav-item dark:bg-white dark:text-gray-900 bg-gray-900 text-white">
+                <a href="">Contact</a>
+              </li>
+            </ul>
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+}
