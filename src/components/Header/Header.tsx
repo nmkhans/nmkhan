80a7d064f367cc4cab1437935ef4af86c78a0349
nmkhans <nurmoin05@gmail.com>
2025-08-13 13:50:00 +0600

    logo added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="container mx-auto relative top-3 px-5 lg:px-0">
       <section className="flex justify-between items-center">
-        <div>hello</div>
+        <div className="font-logo-font text-3xl">NM Khan</div>
         <nav className="hidden lg:flex justify-center items-center grow-1">
           <ul className="w-1/3 flex justify-between items-center gap-1 p-0.5 border dark:border-white/15 rounded-full dark:bg-white/10 bg-white backdrop-blur">
             <li className="nav-item">
